d4a31682b428aecddb6e6e1eddd2d0c6a8db21cf
Pranya Mishra <pranyamishra24@gmail.com>
2026-07-14 09:01:04 +0530

    Added SQLite database integration

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
@@ -8,12 +9,55 @@ CORS(app)
 def submit_feedback():
     data = request.json
 
-    print("\n===== NEW FEEDBACK =====")
-    print(data)
+    conn = sqlite3.connect("feedback.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO feedback
+    (student_name, reg_no, department, semester, course, faculty, feedback, rating)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    """, (
+        data["studentName"],
+        data["regNo"],
+        data["department"],
+        data["semester"],
+        data["course"],
+        data["faculty"],
+        data["feedback"],
+        data["rating"]
+    ))
+
+    conn.commit()
+    conn.close()
 
     return jsonify({
-        "message": "Feedback received successfully!"
+        "message": "Feedback saved successfully!"
     })
 
+
+
+def init_db():
+    conn = sqlite3.connect("feedback.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS feedback(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   student_name TEXT,
+                   reg_no TEXT,
+                   department TEXT,
+                   semester TEXT,
+                   course TEXT,
+                   faculty TEXT,
+                   feedback TEXT,
+                   rating TEXT
+     )               
+    """ )
+
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    init_db()
+    app.run(debug=True)    
+        
